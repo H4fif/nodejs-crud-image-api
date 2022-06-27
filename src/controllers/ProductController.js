da77c92e2c9387e9e0bf2647af544f571e019288
h4fif <hafifimammuddyn@gmail.com>
2022-06-27 09:04:19 +0700

    ✨ add CRUD functionality with image

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,23 +1,122 @@
 const Product = require('../models/ProductModel');
+const path = require('path');
+const fs = require('fs');
 
-const getProducts = (req, res) => {
-
+const getProducts = async (req, res) => {
+  try {
+    const response = await Product.findAll();
+    res.json(response);
+  } catch (error) {
+    console.log('ERROR HERE >>>>>>> ', error);
+  }
 };
 
-const getProductById = (req, res) => {
-
+const getProductById = async (req, res) => {
+  try {
+    const response = await Product.findOne({ where: {id: req.params.id} });
+    res.json(response);
+  } catch (error) {
+    console.log('ERROR HERE >>>>>>> ', error);
+  }
 };
 
 const saveProduct = (req, res) => {
+  try {
+    if (!req.files) return res.status(400).json({ message: 'File required' });
+    const name = req.body.title;
+    const file = req.files.file;
+    const fileSize = file.data.length;
+    const ext = path.extname(file.name)
+    const fileName = file.md5 + ext;
+    const url = `${req.protocol}://${req.get('host')}/images/${fileName}`;
+    const allowedType = ['.png', '.jpg', '.jpeg'];
 
+    if (!allowedType.includes(ext.toLowerCase())) return res.status(422).json({ message: 'invalid image type '});
+    if (fileSize > 5000000) return res.status(422).json({ message: 'image should < 5 MB' });
+
+    const filePath = path.resolve() + '/src/public/images/' + fileName;
+
+    file.mv(filePath, async (error) => {
+      if (error) return res.status(500).json({ message: error.message });
+
+      try {
+        await Product.create({
+          name: name,
+          image: fileName,
+          url: url
+        });
+
+        res.status(201).json({ message: 'success' });
+      } catch (error) {
+        console.log('ERROR UPLOAD IMAGE >>>>>>> ', error.message);
+      }
+    });
+  } catch (error) {
+    console.log('ERROR HERE >>>>>>> ', error);
+  }
 };
 
-const updateProduct = (req, res) => {
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({ where: {id: req.params.id} });
 
+    if (!product) return res.status(404).json({ message: 'Data not found' });
+
+    let fileName = '';
+
+    if (!req.files) {
+      fileName = Product.image;
+    } else {
+      const file = req.files.file;
+      const fileSize = file.data.length;
+      const ext = path.extname(file.name);
+      fileName = file.md5 + ext;
+      const allowedType = ['.png', '.jpg', '.jpeg'];
+
+      if (!allowedType.includes(ext.toLowerCase())) return res.status(422).json({ message: 'invalid image type '});
+      if (fileSize > 5000000) return res.status(422).json({ message: 'image should < 5 MB' });
+
+      const filePath = `${path.resolve()}/src/public/images/${product.image}`;
+      fs.unlinkSync(filePath);
+
+      file.mv(`${path.resolve()}/src/public/images/${fileName}`, (error) => {
+        if (error) return res.status(500).json({ message: error.message });
+
+      });
+    }
+
+    const name = req.body.title;
+    const url = `${req.protocol}://${req.get('host')}/images/${fileName}`;
+
+    await Product.update({
+      name: name,
+      image: fileName,
+      url: url
+    }, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    res.status(200).json({ message: 'success' });
+  } catch (error) {
+    console.log('ERROR HERE >>>>>>> ', error);
+  }
 };
 
-const deleteProduct = (req, res) => {
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({ where: {id: req.params.id} });
 
+    if (!product) return res.status(404).json({ message: 'Data not found' });
+
+    const filePath = `${path.resolve()}/src/public/images/${product.image}`;
+    fs.unlinkSync(filePath);
+    await Product.destroy({where: { id: req.params.id }});
+    res.status(200).json({ message: 'success' });
+  } catch (error) {
+    console.log('ERROR HERE >>>>>>> ', error);
+  }
 };
 
 module.exports = { getProducts, getProductById, saveProduct, updateProduct, deleteProduct };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const FileUpload = require('express-fileupload');
 const cors = require('cors');
 const ProductRoute = require('./routes/ProductRoute');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(FileUpload());
+app.use(express.static(path.resolve() + '/src/public'));
 app.use(ProductRoute);
 
 app.listen(3000, () => console.log('Server up and running...'));
